427d13f90acce47e87625de34a82e4d8003014b2
valenwp <45184460+valenwp@users.noreply.github.com>
2018-11-27 13:41:53 -0600

    inital acceptance of sample command

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,8 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+rejsample<-function(n, pdf, a, b, C){
+  replicate(n, {u<-runif(a,b)
+  v<-runif(0,C)
+  if(pdf(u)>=v){z<-pdf(u)}})
 }
