75b740ff8315133191a414570f3fa132243cbb05
valenwp <45184460+valenwp@users.noreply.github.com>
2018-12-06 10:56:13 -0600

    fixed mcmc

--- R/mcmc sampleing.R
+++ R/mcmc sampleing.R
@@ -7,10 +7,10 @@
 #' @examples
 #' pdf<-function(x){x/2}
 #' t<-runif(1,0,2)
-#' mcmcsample(t, pdf, 3)
-mcmcsample<-function(t, pdf, n){
+#' mcmcsample(t, pdf)
+mcmcsample<-function(t, pdf){
   dat<-t
-  for(i in 1:(n-1)){
+  for(i in 1:0){
   proposed<-t
   if(runif(1,0,1)<pdf(proposed)/pdf(t)){t<-proposed}
   dat<-c(dat,t)
